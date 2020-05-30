8fb9e1eaa6fe2fceaeeb2dce3b0a8265ac83393d
mssen <missy.danielsen@gmail.com>
2020-05-29 22:36:50 -0500

    build: add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CSSReset, ThemeProvider, Box, Text } from "@chakra-ui/core";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider>
+      <CSSReset />
+      <main>
+        <Box
+          border="1px"
+          borderRadius="md"
+          borderColor="gray.300"
+          borderLeftWidth="4px"
+          borderLeftColor="blue.400"
+          m="2"
+          p="2"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Text>Hello</Text>
+        </Box>
+      </main>
+    </ThemeProvider>
   );
 }
 
