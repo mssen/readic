562a75934ab413af203b03005d8bedf984533523
mssen <missy.danielsen@gmail.com>
2020-07-24 20:53:49 -0500

    feat: rough solution to use subcollections

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,41 @@ import { CSSReset, ThemeProvider, Box, Text } from "@chakra-ui/core";
 
 import { firestore } from "./firebase";
 import { collectIdsAndData } from "./utilitis";
+import FicList from "./FicList";
 
 interface UserDoc {
   id: string;
   username: string;
 }
 
+interface FeedDoc {
+  id: string;
+  lastRead: firebase.firestore.Timestamp;
+  url: string;
+  name: string;
+}
+
 function App() {
-  const [users, setUsers] = useState<UserDoc[]>([]);
+  const [user, setUser] = useState<UserDoc>();
+  const [feeds, setFeeds] = useState<FeedDoc[]>([]);
+
+  const [feedRef, setFeedRef] = useState<
+    firebase.firestore.CollectionReference<firebase.firestore.DocumentData>
+  >();
 
   useEffect(() => {
     const fetchTest = async () => {
-      const userSnapshot = await firestore.collection("users").get();
-      setUsers(userSnapshot.docs.map(collectIdsAndData<UserDoc>()));
+      const userRef = firestore.collection("users").doc("VZp12eWFrMFxp5wFzWAd");
+
+      const userDoc = await userRef.get();
+      if (userDoc.exists) {
+        setUser(userDoc.data() as UserDoc);
+      }
+
+      const ref = userRef.collection("feeds");
+      setFeedRef(ref);
+      const feedSnapshot = await ref.get();
+      setFeeds(feedSnapshot.docs.map(collectIdsAndData<FeedDoc>()));
     };
     fetchTest();
   }, []);
@@ -24,19 +46,24 @@ function App() {
     <ThemeProvider>
       <CSSReset />
       <main>
-        {users.map((user) => (
-          <Box
-            key={user.id}
-            border="1px"
-            borderRadius="md"
-            borderColor="gray.300"
-            borderLeftWidth="4px"
-            borderLeftColor="blue.400"
-            m="2"
-            p="2"
-          >
-            <Text>{user.username}</Text>
-          </Box>
+        <Box
+          border="1px"
+          borderRadius="md"
+          borderColor="gray.300"
+          borderLeftWidth="4px"
+          borderLeftColor="blue.400"
+          m="2"
+          p="2"
+        >
+          <Text>{user?.username ?? "No user found"}</Text>
+        </Box>
+        {feeds.map((feed) => (
+          <React.Fragment key={feed.id}>
+            <Text>
+              {feed.name} was last read at {feed.lastRead.toDate().toString()}
+            </Text>
+            <FicList feedId={feed.id} feedRef={feedRef!} />
+          </React.Fragment>
         ))}
       </main>
     </ThemeProvider>
---
+++ src/FicList.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import { Box } from "@chakra-ui/core";
+
+import { collectIdsAndData } from "./utilitis";
+
+interface Props {
+  feedRef: firebase.firestore.CollectionReference<
+    firebase.firestore.DocumentData
+  >;
+  feedId: string;
+}
+
+interface FicAuthor {
+  link: string;
+  name: string;
+}
+
+interface FicChapters {
+  current: number;
+  total: number | null;
+}
+
+interface FicDoc {
+  id: string;
+  author: FicAuthor;
+  categories: Record<string, string>[];
+  chapters: FicChapters;
+  characters: Record<string, string>[];
+  language: string;
+  published: firebase.firestore.Timestamp;
+  reating: string;
+  relationships: Record<string, string>[];
+  summary: string;
+  tags: Record<string, string>[];
+  title: string;
+  updated: firebase.firestore.Timestamp;
+  url: string;
+  warnings: Record<string, string>[];
+  words: number;
+}
+
+const FicList = ({ feedRef, feedId }: Props) => {
+  const [fics, setFics] = useState<FicDoc[]>([]);
+
+  useEffect(() => {
+    const fetchFics = async () => {
+      const ficsSnapshot = await feedRef.doc(feedId).collection("fics").get();
+      setFics(ficsSnapshot.docs.map(collectIdsAndData<FicDoc>()));
+    };
+    fetchFics();
+  });
+
+  return (
+    <React.Fragment>
+      {fics.map((fic) => (
+        <Box
+          border="1px"
+          borderRadius="md"
+          borderColor="gray.300"
+          borderLeftWidth="4px"
+          borderLeftColor="blue.400"
+          m="2"
+          p="2"
+          key={fic.id}
+        >
+          {fic.title}
+        </Box>
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default FicList;
